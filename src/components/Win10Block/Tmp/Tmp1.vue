c1f22ca29c04c52856a73cbb8640a0289415db76
shifeng <shifeng199307@gmail.com>
2018-05-08 13:37:28 +0800

    win10风格框架

---
+++ src/components/Win10Block/Tmp/Tmp1.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="win10-block-tmp tmp1" :style="{backgroundColor: model.backgroundColor}">
+    <img src="../../../assets/logo.png">
+    <p>helloworld</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Tmp1',
+  data() {
+    return {
+    }
+  },
+  props: {
+    model: {
+      type: Object
+    }
+  },
+  mounted() {
+  }
+}
+</script>
+
+<style scoped>
+.win10-block-tmp.tmp1 img{
+  width: 50%;
+  height: 50%;
+  margin: 10px
+}
+.win10-block-tmp.tmp1 p{
+  margin: 0;
+}
+</style>
---
+++ src/components/Win10Block/Tmp/Tmp7.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="tmp7">
+    Tmp7
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Tmp7',
+  data() {
+    return {
+    }
+  },
+  mounted() {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
